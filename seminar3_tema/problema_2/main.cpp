b7b3a1e6f5cd60acf625037fef06ed4de43c2fed
apersephone <83142073+apersephone@users.noreply.github.com>
2022-03-13 14:47:14 +0200

    tema3

--- seminar3_tema/problema_2/main.cpp
+++ seminar3_tema/problema_2/main.cpp
@@ -3,12 +3,15 @@
 
 int main() {
     Canvas C = {10, 10};
-//    C.DrawRect(2, 2 ,9,9, '*');
-//    C.Print();
-//    std::cout << '\n';
-//    C.Clear();
-//    C.FillRect(2, 2 ,9,9, '*');
-//    C.Print();
+    C.DrawRect(2, 2 ,9,9, '*');
+    C.Print();
+    std::cout << '\n';
+    C.Clear();
+    C.FillRect(2, 2 ,9,9, '*');
+    C.Print();
+    C.Clear();
+    std::cout << "\n";
+
     C.DrawCircle(5, 5, 3, '*');
     C.Print();
 
